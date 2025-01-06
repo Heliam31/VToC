9ffbe529617b2661348b44f4d6b24a24c422aeca
Mihai Poitelea <mihaipoitelea11@gmail.com>
2025-01-06 16:50:04 +0100

    Code working, need to add phrases

--- Py/mainLITEST.py
+++ Py/mainLITEST.py
@@ -12,40 +12,52 @@ import keyboard
 import pyautogui
 from pynput.mouse import Button, Controller, Listener
 import Levenshtein
+import duckdb as dd
+
 
 
 def on_click(x, y, button, pressed):
     if button == Button.x1:  # MB4 correspond généralement à Button.x1
         if pressed:
             user_message =  speech_to_text()
-            print("message : ",user_message)
             start = time()
             phrase = user_message.lower().translate(str.maketrans("","", string.punctuation)).replace(' ', '')
-            print("apres split : ",phrase)
+
             if user_message.lower().translate(str.maketrans("","", string.punctuation)).replace(' ', '') == "aurevoir":
-                print("labite")
+                print("Exit message detecté, bye")
                 exit(1)
-            ouvre = ["ouvrezcetteporte","f5", "1"]
-            croche = ["crochetezcette porte","f6", "1"]
-            MLi = [ouvre,croche]
-            smallerdist = [-1,-1]
-            for i in range(len(MLi)):
-                distance = Levenshtein.distance(phrase, MLi[i][0])
-                print(f"Distance de Levenshtein : {distance}")
-                if smallerdist == [-1,-1]:
-                    smallerdist = [i,distance]
+
+            #Base de phrases
+            con = dd.connect('my_database.db')
+            try:
+                ph_list = con.execute('SELECT phrase FROM phrases').fetchall()
+            except dd.CatalogException:
+                print("[ERROR] Base de données vide, créez votre base de phrases")
+
+            #Comparaison de notre phrase à celles de la base
+            smallerdist = ["",-1]
+            for ph in ph_list:
+                distance = Levenshtein.distance(phrase, ph[0])
+                # print(f"Distance de Levenshtein : {distance}")
+                if smallerdist == ["",-1]:
+                    smallerdist = [ph[0],distance]
                 if distance == 0:
-                    smallerdist = [i,distance]
+                    smallerdist = [ph[0],distance]
                     break
                 if distance < smallerdist[1]:
-                    smallerdist = [i, distance]
+                    smallerdist = [ph[0], distance]
                 
-            if smallerdist != [-1,-1] and smallerdist[1] < 10:
+            print(f"phrase sélectionnée : {smallerdist[0]} avec confiance {smallerdist[1]}")
+
+            # Si on a selectionné une phrase de la base et que sa distance est < 10 mots
+            if smallerdist != ["",-1] and smallerdist[1] < 10:
                 mouse = Controller()
                 mouse.click(Button.middle)
 
-                for zz in MLi[smallerdist[0]][1:]:
-                    print("le ziziz ", zz)
+                res = con.execute("SELECT commands FROM phrases WHERE phrase = \'" + smallerdist[0] + "\';").fetchall()
+
+                for zz in res[0][0]:
+                    print("pressing :  ", zz)
                     pyautogui.press(zz)
 
             print(f"\n-------------------------Generated in {(time()-start)}s.-------------------------")
