db31a1e8e6033599c75ed2118cda1abd67d92675
Mihai Poitelea <mihaipoitelea11@gmail.com>
2025-01-12 12:23:14 +0100

    create branch cpp

---
+++ cpp/build/CMakeFiles/MonExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MonExecutable.dir/src/main.cpp.obj"
+  "CMakeFiles/MonExecutable.dir/src/main.cpp.obj.d"
+  "MonExecutable.exe"
+  "MonExecutable.exe.manifest"
+  "MonExecutable.pdb"
+  "libMonExecutable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MonExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/build/CMakeFiles/record.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/record.dir/src/record.cpp.obj"
+  "CMakeFiles/record.dir/src/record.cpp.obj.d"
+  "librecord.dll.a"
+  "record.exe"
+  "record.exe.manifest"
+  "record.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/record.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
